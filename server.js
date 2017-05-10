ca81c2323c87828ce26b7019b1a4404343e0dd9f
Paul Green <thatgreenguy@gmx.co.uk>
2017-05-10 19:48:33 +0100

    Ok next

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ var stylizer = require('stylizer');
 var templatizer = require('templatizer');
 var app = express();
 
-// Checking auto lint on git hook add crap line
-if ( x == y 
-) ;;
 
 // a little helper for fixing paths for various environments
 var fixPath = function (pathString) {
